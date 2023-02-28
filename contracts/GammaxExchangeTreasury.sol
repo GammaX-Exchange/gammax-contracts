8f5ddc6870b1b22b80c884889a360b77ec04f210
unknown <ppmunga@gmail.com>
2023-02-28 13:21:43 +0300

    Removed ECDSA import

--- contracts/GammaxExchangeTreasury.sol
+++ contracts/GammaxExchangeTreasury.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 pragma experimental ABIEncoderV2;
@@ -129,5 +128,4 @@ contract GammaxExchangeTreasury is Ownable,ReentrancyGuard {
         delete supportCurrency[currency];
         emit RemoveCurrency(currency);
     }
-
 }
